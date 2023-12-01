177d2225b579dd2e89797b294eb28a1b4b7dce11
Gieun <naru_oo@naver.com>
2023-12-01 15:54:52 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,20 @@
-import { Inter } from 'next/font/google';
-import { Map } from '@/components/Map';
-import RootLayout from '@/layout/RootLayout';
-import Sidebar from '@/components/Sidebar';
+import type { AppProps } from 'next/app';
+import { Noto_Sans_KR } from 'next/font/google';
+import classNames from 'classnames';
 
-const inter = Inter({ subsets: ['latin'] });
+import '@/styles/globals.scss';
 
-export default function Main() {
+const notoSansKr = Noto_Sans_KR({
+  // preload: true, 기본값
+  subsets: ['latin'], // 또는 preload: false
+  weight: ['100', '400', '500', '700', '900'], // 가변 폰트가 아닌 경우, 사용할 fontWeight 배열
+  variable: '--notoSans', // CSS 변수 방식으로 스타일을 지정할 경우에 사용합니다.
+});
+
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RootLayout>
-      <section
-        className={`w-[100%] h-screen flex flex-col items-center justify-between ${inter.className}`}
-      >
-        <div className="w-full h-full">
-          위드 테스트위드 테스트위드 테스트위드 테스트위드 테스트위드 테스트위드
-          테스트위드 테스트위드 테스트위드 테스트 위드 테스트위드 테스트위드
-          테스트위드 테스트위드 테스트위드 테스트위드 테스트위드 테스트위드
-          테스트위드 테스트 위드 테스트위드 테스트위드 테스트위드 테스트위드
-          테스트위드 테스트위드 테스트위드 테스트위드 테스트위드 테스트
-          <Map />
-        </div>
-      </section>
-      <Sidebar />
-    </RootLayout>
+    <main className={classNames(notoSansKr.className, notoSansKr.variable)}>
+      <Component {...pageProps} />
+    </main>
   );
 }
