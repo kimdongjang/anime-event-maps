d2790e061e5c946059b5a22c5956c2aeca1ae772
Gieun <naru_oo@naver.com>
2023-12-01 14:10:07 +0900

    Update index.tsx

--- src/layout/RootLayout/index.tsx
+++ src/layout/RootLayout/index.tsx
@@ -1,3 +1,21 @@
-export const RootLayout = () => {
+import Head from 'next/head';
 
-}
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const siteTitle = '애니메이션 행사 맵스';
+  const description = `
+  this web serivce is developing, provides SNS, Comission, Funding
+  service. If you are interested, please send to gieunp3644@gmail.com.
+  `;
+
+  return (
+    <>
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Croissants" />
+        <meta property="og:title" content={siteTitle} />
+      </Head>
+      <div className="w-full h-full flex">{children}</div>
+    </>
+  );
+};
+export default RootLayout;
