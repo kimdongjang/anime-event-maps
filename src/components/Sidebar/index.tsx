80f8f693584df6efbb5d1cd57cdf0d9529c38b64
Gieun <naru_oo@naver.com>
2023-12-01 17:32:24 +0900

    Update index.tsx

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -8,6 +8,7 @@ import styles from './Sidebar.module.scss';
 import { EventSummary } from '../Event/Summary';
 import { SearchBox } from '../SearchBox';
 import { IEvent } from '@/services/event/@types';
+import { sampleEvents } from '@/constants/sample';
 
 interface ISidebar {
   handleShow: any;
@@ -38,47 +39,20 @@ const Sidebar = (props: ISidebar) => {
     }
   };
 
-  const sampleEvents: IEvent[] = [
-    {
-      title: '이터널리티 행사',
-      category: '게임행사',
-      startDate: '2024-02-01',
-      endDate: '2024-02-04',
-    },
-    {
-      title: '이터널리티 행사',
-      category: '게임행사',
-      startDate: '2024-02-01',
-      endDate: '2024-02-04',
-    },
-    {
-      title: '이터널리티 행사',
-      category: '게임행사',
-      startDate: '2024-02-01',
-      endDate: '2024-02-04',
-    },
-    {
-      title: '이터널리티 행사',
-      category: '게임행사',
-      startDate: '2024-02-01',
-      endDate: '2024-02-04',
-    },
-  ];
-
   const renderEventList = () => {
     return sampleEvents.map((data, i) => <EventSummary event={data} key={i} />);
   };
   return (
-    <aside className={`fixed flex h-full`}>
+    <aside className={`fixed flex h-full `}>
       <div
-        className={`bg-gray-50 h-full w-[30%] flex ${
+        className={`bg-gray-50 h-full w-0 md:w-[45%] lg:w-[40%] xl:w-[35%] flex overflow-y-scroll ${
           isShow ? styles.show_side_bar : styles.close_side_bar
         }`}
       >
-        <div className="relative ">{renderBtn()}</div>
+        <div className="sticky top-0">{renderBtn()}</div>
         <div className="flex flex-col w-full">
           <div className="flex items-center m-3">
-            <SearchBox className="w-full text-lg" />
+            <SearchBox className="w-full text-lg " />
             <MdFilterAlt size={32} className="text-gray-400 ml-1" />
           </div>
           {renderEventList()}
