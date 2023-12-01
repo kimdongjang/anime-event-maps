d1c6b43c24e60d85035d63a4a6f655f4916218f0
Gieun <naru_oo@naver.com>
2023-12-01 15:39:55 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Noto_Sans_KR } from 'next/font/google';
+import classNames from 'classnames';
+
+const notoSansKr = Noto_Sans_KR({
+  // preload: true, 기본값
+  subsets: ['latin'], // 또는 preload: false
+  weight: ['100', '400', '500', '700', '900'], // 가변 폰트가 아닌 경우, 사용할 fontWeight 배열
+  variable: '--notoSans', // CSS 변수 방식으로 스타일을 지정할 경우에 사용합니다.
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={classNames(notoSansKr.className, notoSansKr.variable)}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
