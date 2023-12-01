1619eba5e6fbba653a5ae77b733192b19970e9f4
Gieun <naru_oo@naver.com>
2023-12-01 16:58:23 +0900

    Create sample.ts

---
+++ src/constants/sample.ts
@@ -0,0 +1,28 @@
+import { IEvent } from '@/services/event/@types';
+
+export const sampleEvents: IEvent[] = [
+  {
+    title: '이터널리티 행사',
+    category: '게임행사',
+    startDate: '2024-02-01',
+    endDate: '2024-02-04',
+  },
+  {
+    title: '이터널리티 행사',
+    category: '게임행사',
+    startDate: '2024-02-01',
+    endDate: '2024-02-04',
+  },
+  {
+    title: '이터널리티 행사',
+    category: '게임행사',
+    startDate: '2024-02-01',
+    endDate: '2024-02-04',
+  },
+  {
+    title: '이터널리티 행사',
+    category: '게임행사',
+    startDate: '2024-02-01',
+    endDate: '2024-02-04',
+  },
+];
