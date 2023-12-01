0c21e02f07f68b74274815850f17ccb7172596c9
Gieun <naru_oo@naver.com>
2023-12-01 14:34:48 +0900

    Create index.tsx

---
+++ src/services/event/@types/index.tsx
@@ -0,0 +1,15 @@
+import { IImage } from '../../../../constants/common';
+
+export interface IEvent {
+  title: string;
+  category: string;
+  startDate: string;
+  endDate: string;
+
+  images: IImage[];
+
+  minPrice?: number;
+  maxPrice?: number;
+  jibunAddress?: string;
+  doroAddress?: string;
+}
