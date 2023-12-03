50114d3509cf9925b83eef23ed76dc4b9932e63f
JeonWoonKi <naru_oo@naver.com>
2023-12-03 22:20:59 +0900

    스타일 업데이트중

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
 const CompressionPlugin = require('compression-webpack-plugin');
+const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   // webpack: (config) => {
   //   // css loader
   //   config.module.rules.push({
