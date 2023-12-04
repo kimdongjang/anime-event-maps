109dd61d6a21e64baab3ad2bd05f406fbff64bb3
Gieun <naru_oo@naver.com>
2023-12-04 17:51:25 +0900

    Update useMap.tsx

--- src/hooks/useMap.tsx
+++ src/hooks/useMap.tsx
@@ -1,30 +1,43 @@
 import { useCallback } from 'react';
 import type { Coordinates, NaverMap } from '../types/map';
-import { useMutation } from 'react-query';
+import useSWR, { mutate } from 'swr';
+
+export const INITIAL_CENTER: Coordinates = {
+  lat: 37.4862618,
+  lng: 127.1222903,
+};
+export const INITIAL_ZOOM = 11;
+
+export const MAP_KEY = '/map';
 
 const useMapHook = () => {
-    // const { data: map } = useMutation(MAP_KEY);
+  /** useSWR의 MAP_KEY로 API를 지정 */
+  const { data: map } = useSWR(MAP_KEY);
 
-    // const initializeMap = useCallback((map: NaverMap) => {
-    //     mutate(MAP_KEY, map);
-    // }, []);
+  const initializeMap = useCallback((map: NaverMap) => {
+    /** mutate로 MAP_KEY를 호출하는 것으로 naver map 인스턴스를 가져올 수 있다고함 */
+    mutate(MAP_KEY, map);
+  }, []);
 
-    // const resetMapOptions = useCallback(() => {
-    //     map.morph(new naver.maps.LatLng(...INITIAL_CENTER), INITIAL_ZOOM);
-    // }, [map]);
+  const resetMapOptions = useCallback(() => {
+    map.morph(
+      new naver.maps.LatLng(INITIAL_CENTER.lat, INITIAL_CENTER.lng),
+      INITIAL_ZOOM
+    );
+  }, [map]);
 
-    // const getMapOptions = useCallback(() => {
-    //     const mapCenter = map.getCenter();
-    //     const center: Coordinates = [mapCenter.lat(), mapCenter.lng()];
-    //     const zoom = map.getZoom();
+  const getMapOptions = useCallback(() => {
+    const mapCenter = map.getCenter();
+    const center = [mapCenter.lat(), mapCenter.lng()];
+    const zoom = map.getZoom();
 
-    //     return { center, zoom };
-    // }, [map]);
+    return { center, zoom };
+  }, [map]);
 
-    // return {
-    //     initializeMap,
-    //     resetMapOptions,
-    //     getMapOptions,
-    // };
+  return {
+    initializeMap,
+    resetMapOptions,
+    getMapOptions,
+  };
 };
 export default useMapHook;
