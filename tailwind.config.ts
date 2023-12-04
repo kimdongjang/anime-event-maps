e2c7ecec7136994e5763b0d2457b68eb782a8529
Gieun <naru_oo@naver.com>
2023-12-04 11:39:44 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -16,8 +19,8 @@ const config: Config = {
     },
     fontFamily: {
       NotoSans: ['Noto Sans KR'],
-    }
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
