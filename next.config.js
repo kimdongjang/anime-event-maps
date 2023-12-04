0ab83dabe96b3999f9aa8c1be50f89c0fb0ae00a
Gieun <naru_oo@naver.com>
2023-12-04 11:38:36 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,31 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
 const CompressionPlugin = require('compression-webpack-plugin');
-const path = require('path')
+const path = require('path');
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+  // styles 폴더와 같은 공간에 모아두고 싶은 경우에 해제
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')],
+  // },
   // webpack: (config) => {
   //   // css loader
-  //   config.module.rules.push({
-  //     test: /\.css$/i,
-  //     use: ['style-loader', 'css-loader'],
-  //   });
+  //   config.module.rules.push(
+  //     {
+  //       test: /\.css$/i,
+  //       use: ['style-loader', 'css-loader', 'postcss-loader'],
+  //     },
+  //     {
+  //       test: /\.(scss|sass)$/i,
+  //       include: [path.resolve(__dirname, 'node_modules')],
+  //       use: ['css-loader', 'sass-loader'],
+  //     }
+  //   );
   //   config.plugins.push(new CompressionPlugin());
   //   return config;
   // },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
